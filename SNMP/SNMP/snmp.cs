e8e78d494ebe0eef61e591ddb96a4e775d1ee8c2
menewol <wolfgang@menezes.at>
2014-01-30 12:41:27 +0100

    snmp class/pdu class

---
+++ SNMP/SNMP/snmp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNMP
+{
+    
+    class snmp
+    {
+        const string CommunityString = "ntma_snmp";
+        byte[] version = new byte[4];
+        private snmp(int versionNumber)
+        {
+            version = Encoding.Default.GetBytes(versionNumber.ToString());
+        }
+
+    }
+
+}
